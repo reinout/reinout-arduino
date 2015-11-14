759086961b1a276e651e29c9aff18377cedcc338
Reinout van Rees <reinout@vanrees.org>
2015-11-14 18:01:02 +0100

    Added good parameters for both servos, but they don't work together yet

--- current-test-program/servo_via_timer.ino
+++ current-test-program/servo_via_timer.ino
@@ -1,38 +1,70 @@
 #include <Servo.h>
 
-Servo servo1;
+Servo main_signal;
+Servo secondary_signal;
 
-int SERVO1_MIN = 1250;
-int SERVO1_MAX = 1750;
-unsigned long SERVO1_DURATION_MS = 1200;
-unsigned long servo1_timestep_micros;
-unsigned long servo1_next_time_micros;
-int servo1_value;
-unsigned long INITIAL_DELAY_MS = 500;
+int MAIN_SIGNAL_MIN = 1300;
+int MAIN_SIGNAL_MAX = 1750;
+int MAIN_SIGNAL_DURATION_MS = 1200;
+int main_signal_timestep_ms;
+unsigned int main_signal_next_time_ms;
+int main_signal_value;
+
+int SECONDARY_SIGNAL_MIN = 1150;
+int SECONDARY_SIGNAL_MAX = 1750;
+int SECONDARY_SIGNAL_DURATION_MS = 1200;
+int secondary_signal_timestep_ms;
+unsigned int secondary_signal_next_time_ms;
+int secondary_signal_value;
+
+int INITIAL_DELAY_MS = 500;
 
 
 void setup() {
-  servo1.attach(11);
   pinMode(13, OUTPUT);
-  servo1_timestep_micros = (SERVO1_DURATION_MS * 1000) / (SERVO1_MAX - SERVO1_MIN);
-  servo1_value = SERVO1_MIN;
-  servo1.writeMicroseconds(servo1_value);
-  servo1_next_time_micros = micros() + 1000 * INITIAL_DELAY_MS;
+
+  // main_signal.attach(11);
+  // main_signal_timestep_ms = (MAIN_SIGNAL_DURATION_MS) / (MAIN_SIGNAL_MAX - MAIN_SIGNAL_MIN);
+  // main_signal_value = MAIN_SIGNAL_MIN;
+  // main_signal.writeMicroseconds(main_signal_value);
+  // main_signal_next_time_ms = millis() + INITIAL_DELAY_MS;
+  // main_signal.writeMicroseconds(main_signal_value);
+
+  secondary_signal.attach(12);
+  secondary_signal_timestep_ms = (SECONDARY_SIGNAL_DURATION_MS) / (SECONDARY_SIGNAL_MAX - SECONDARY_SIGNAL_MIN);
+  secondary_signal_value = SECONDARY_SIGNAL_MIN;
+  secondary_signal.writeMicroseconds(secondary_signal_value);
+  secondary_signal_next_time_ms = millis() + INITIAL_DELAY_MS + 50;
+  secondary_signal.writeMicroseconds(secondary_signal_value);
+
 }
 
 
-void perhaps_update_servo1() {
-  if (micros() > servo1_next_time_micros) {
-    servo1_value += 1;
-    if (servo1_value > SERVO1_MAX) {
-      servo1.detach();
+void perhaps_update_main_signal() {
+  if (millis() > main_signal_next_time_ms) {
+    main_signal_value += 1;
+    if (main_signal_value > MAIN_SIGNAL_MAX) {
+      main_signal.detach();
     }
-    servo1_next_time_micros += servo1_timestep_micros;
-    servo1.writeMicroseconds(servo1_value);
+    main_signal_next_time_ms += main_signal_timestep_ms;
+    main_signal.writeMicroseconds(main_signal_value);
+  }
+}
+
+
+void perhaps_update_secondary_signal() {
+  if (millis() > secondary_signal_next_time_ms) {
+    secondary_signal_value += 1;
+    if (secondary_signal_value > SECONDARY_SIGNAL_MAX) {
+      secondary_signal.detach();
+    }
+    secondary_signal_next_time_ms += secondary_signal_timestep_ms;
+    secondary_signal.writeMicroseconds(secondary_signal_value);
   }
 }
 
 
 void loop() {
-  perhaps_update_servo1();
+  // perhaps_update_main_signal();
+  perhaps_update_secondary_signal();
 }
