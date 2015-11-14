350e9c180e63e9d16a5c8bb9c97010966ef80983
Reinout van Rees <reinout@vanrees.org>
2015-11-14 19:53:15 +0100

    Enabling both again

--- current-test-program/servo_via_timer.ino
+++ current-test-program/servo_via_timer.ino
@@ -5,14 +5,14 @@ Servo secondary_signal;
 
 int MAIN_SIGNAL_MIN = 1300;
 int MAIN_SIGNAL_MAX = 1750;
-int MAIN_SIGNAL_DURATION_MS = 1200;
+int MAIN_SIGNAL_DURATION_MS = 2200;
 int main_signal_timestep_ms;
 unsigned int main_signal_next_time_ms;
 int main_signal_value;
 
 int SECONDARY_SIGNAL_MIN = 1150;
 int SECONDARY_SIGNAL_MAX = 1750;
-int SECONDARY_SIGNAL_DURATION_MS = 1200;
+int SECONDARY_SIGNAL_DURATION_MS = 2200;
 int secondary_signal_timestep_ms;
 unsigned int secondary_signal_next_time_ms;
 int secondary_signal_value;
@@ -23,12 +23,12 @@ int INITIAL_DELAY_MS = 500;
 void setup() {
   pinMode(13, OUTPUT);
 
-  // main_signal.attach(11);
-  // main_signal_timestep_ms = (MAIN_SIGNAL_DURATION_MS) / (MAIN_SIGNAL_MAX - MAIN_SIGNAL_MIN);
-  // main_signal_value = MAIN_SIGNAL_MIN;
-  // main_signal.writeMicroseconds(main_signal_value);
-  // main_signal_next_time_ms = millis() + INITIAL_DELAY_MS;
-  // main_signal.writeMicroseconds(main_signal_value);
+  main_signal.attach(11);
+  main_signal_timestep_ms = (MAIN_SIGNAL_DURATION_MS) / (MAIN_SIGNAL_MAX - MAIN_SIGNAL_MIN);
+  main_signal_value = MAIN_SIGNAL_MIN;
+  main_signal.writeMicroseconds(main_signal_value);
+  main_signal_next_time_ms = millis() + INITIAL_DELAY_MS;
+  main_signal.writeMicroseconds(main_signal_value);
 
   secondary_signal.attach(12);
   secondary_signal_timestep_ms = (SECONDARY_SIGNAL_DURATION_MS) / (SECONDARY_SIGNAL_MAX - SECONDARY_SIGNAL_MIN);
@@ -65,6 +65,6 @@ void perhaps_update_secondary_signal() {
 
 
 void loop() {
-  // perhaps_update_main_signal();
+  perhaps_update_main_signal();
   perhaps_update_secondary_signal();
 }
