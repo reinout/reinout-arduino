936f75f9042909fc84a080f05958517878c8199c
Reinout van Rees <reinout@vanrees.org>
2015-11-30 20:12:13 +0100

    Configured single arm signal

--- current-test-program/servo_via_timer.ino
+++ current-test-program/servo_via_timer.ino
@@ -1,49 +1,18 @@
 #include <Servo.h>
 #include <servomover.h>
+#include <signalmover.h>
 #include <Bounce2.h>
 
-LinearServoMover upper_arm = LinearServoMover(11, 1301, 1750, 1200);
-LinearServoMover lower_arm = LinearServoMover(12, 1150, 1750, 1200);
+LinearServoMover upper_arm = LinearServoMover(4, 1201, 1850, 800);
 
-Bounce hp1_button = Bounce();
-Bounce hp2_button = Bounce();
-
-int INITIAL_DELAY_MS = 2000;
+SingleSignalMover signal = SingleSignalMover(&upper_arm, 2);
 
 
 void setup() {
-  hp1_button.attach(2, INPUT_PULLUP);
-  hp2_button.attach(3, INPUT_PULLUP);
-  hp1_button.interval(20);
-  hp2_button.interval(20);
-
-  upper_arm.init();
-  lower_arm.init();
+  signal.init();
 }
 
 
 void loop() {
-  hp1_button.update();
-  hp2_button.update();
-
-  if (hp1_button.rose() ) {
-    upper_arm.move_to_min();
-    lower_arm.move_to_min();
-  }
-  if (hp1_button.fell() ) {
-    upper_arm.move_to_max();
-    lower_arm.move_to_min();
-  }
-
-  if (hp2_button.rose() ) {
-    upper_arm.move_to_min();
-    lower_arm.move_to_min();
-  }
-  if (hp2_button.fell() ) {
-    upper_arm.move_to_max();
-    lower_arm.move_to_max();
-  }
-
-  upper_arm.perhaps_update();
-  lower_arm.perhaps_update();
+  signal.perhaps_update();
 }
