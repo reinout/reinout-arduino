f2d11d7fe70de2256f0a82b652a8fa3e4a2505e7
Reinout van Rees <reinout@vanrees.org>
2015-12-02 10:40:38 +0100

    semiconlons...

--- signals/berenbach_a/berenbach_a.ino
+++ signals/berenbach_a/berenbach_a.ino
@@ -5,8 +5,8 @@
 
 LinearServoMover upper_arm = LinearServoMover(11, 1301, 1750, 1200);
 LinearServoMover lower_arm = LinearServoMover(12, 1150, 1750, 1200);
-int HP1_BUTTON_PIN = 2
-int HP2_BUTTON_PIN = 3
+int HP1_BUTTON_PIN = 2;
+int HP2_BUTTON_PIN = 3;
 
 DualSignalMover signal = DualSignalMover(&upper_arm, HP1_BUTTON_PIN,
                                          &lower_arm, HP2_BUTTON_PIN);
