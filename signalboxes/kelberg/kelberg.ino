686a3e942f3b973e879dacaecfc300215e39d309
Reinout van Rees <reinout@vanrees.org>
2016-05-02 22:40:20 +0200

    Starting new project for Kelberg's signalbox

---
+++ signalboxes/kelberg/kelberg.ino
@@ -0,0 +1,46 @@
+#include <servomover.h>  /* Only for library detection */
+#include <Servo.h>  /* Only for library detection */
+#include <Bounce2.h>
+
+// Red LEDs named after the signal/switch lever/etc they're for.
+int LED_LEVER_SWITCH_5 = 1;
+int LED_LEVER_SWITCH_1 = 1;
+int LED_LEVER_SIGNAL_A2 = 1;
+int LED_LEVER_SIGNAL_A1 = 1;
+int LED_SWITCHING_PERMISSION_LOCK = 1;
+int LED_LEVER_ROUTE_A = 1;
+int LED_LEVER_ROUTE_BERENBACH = 1;
+
+// White LED
+int LED_ROUTE_A_FIXED = 1;
+
+// Levers and lock
+int LEVER_SWITCH_5 = 1;
+int LEVER_SWITCH_1 = 1;
+int LEVER_SIGNAL_A2 = 1;
+int LEVER_SIGNAL_A1 = 1;
+int SWITCHING_PERMISSION_LOCK = 1;
+
+// Route levers
+int LEVER_ROUTE_A1 = 1;
+int LEVER_ROUTE_A2 = 1;
+int LEVER_ROUTE_BERENBACH1 = 1;
+int LEVER_ROUTE_BERENBACH2 = 1;
+
+// Push button for route fixing
+int BUTTON_ROUTE_A_FIXED = 1;
+
+// Output pins (pull to 0 means go out of the ground position)
+int OUTPUT_SWITCH_5 = 1;
+int OUTPUT_SWITCH_1 = 1;
+int OUTPUT_SIGNAL_A1 = 1;
+int OUTPUT_SIGNAL_A2 = 1;
+
+
+
+void setup() {
+}
+
+
+void loop() {
+}
