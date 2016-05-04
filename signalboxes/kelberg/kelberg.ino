332ee3558a15d180cf593d8559144604e9fc3cb8
Reinout van Rees <reinout@vanrees.org>
2016-05-04 02:54:22 +0200

    non-working versio

--- signalboxes/kelberg/kelberg.ino
+++ signalboxes/kelberg/kelberg.ino
@@ -2,37 +2,36 @@
 // #include <Servo.h>  /* Only for library detection */
 #include <Bounce2.h>
 
-// Pins
-// ====
-//
 // Red LEDs named after the signal/switch lever/etc they're for.
-int LED_SWITCH_5 = 1;
-int LED_SWITCH_1 = 1;
-int LED_SIGNAL_A2 = 1;
-int LED_SIGNAL_A1 = 1;
-int LED_SWITCHING_PERMISSION = 1;
-int LED_ROUTE_A = 1;
-int LED_ROUTE_BERENBACH = 1;
+// Labeled as a8-a15, so 89-82
+int PIN_LED_SWITCH_5 = 89;  // a8
+int PIN_LED_SWITCH_1 = 88;
+int PIN_LED_SIGNAL_A2 = 87;
+int PIN_LED_SIGNAL_A1 = 86;
+int PIN_LED_SWITCHING_PERMISSION = 85;
+int PIN_LED_ROUTE_A = 84;
+int PIN_LED_ROUTE_BERENBACH = 83;
 // White LED
-int LED_ROUTE_A_FIXED = 1;
+int PIN_LED_ROUTE_A_FIXATION = 82;  // a15
+
 // Levers and lock
-int LEVER_SWITCH_5 = 1;
-int LEVER_SWITCH_1 = 1;
-int LEVER_SIGNAL_A2 = 1;
-int LEVER_SIGNAL_A1 = 1;
-int LOCK_SWITCHING_PERMISSION = 1;
+int PIN_LEVER_SWITCH_5 = 78;  // d22
+int PIN_LEVER_SWITCH_1 = 76;  // d24
+int PIN_LEVER_SIGNAL_A2 = 74;  // d26
+int PIN_LEVER_SIGNAL_A1 = 72;  // d28
+int PIN_LOCK_SWITCHING_PERMISSION = 58;  // d32
 // Route levers
-int LEVER_ROUTE_A1 = 1;
-int LEVER_ROUTE_A2 = 1;
-int LEVER_ROUTE_BERENBACH1 = 1;
-int LEVER_ROUTE_BERENBACH2 = 1;
+int PIN_LEVER_ROUTE_A1 = 54;  // d36
+int PIN_LEVER_ROUTE_A2 = 50;  // d38
+int PIN_LEVER_ROUTE_BERENBACH1 = 52;  // d40
+int PIN_LEVER_ROUTE_BERENBACH2 = 42;  // d42
 // Push button for route fixing
-int BUTTON_ROUTE_A_FIXED = 1;
+int PIN_BUTTON_ROUTE_A_FIXATION = 38;  // d46
 // Output pins (pull to 0 means go out of the ground position)
-int OUTPUT_SWITCH_5 = 1;
-int OUTPUT_SWITCH_1 = 1;
-int OUTPUT_SIGNAL_A1 = 1;
-int OUTPUT_SIGNAL_A2 = 1;
+int PIN_OUTPUT_SWITCH_5 = 6;  // pwm 2 TODO
+int PIN_OUTPUT_SWITCH_1 = 7;  // pwm 3 TODO
+int PIN_OUTPUT_SIGNAL_A1 = 1;  // pwm 4 TODO
+int PIN_OUTPUT_SIGNAL_A2 = 5;  // pwm 5 TODO
 
 // Array index numbers. These are columns in the locking table.
 int SWITCH_5 = 0;
@@ -42,7 +41,7 @@ int ROUTE_A1 = 3;
 int ROUTE_A2 = 4;
 int ROUTE_BERENBACH1 = 5;
 int ROUTE_BERENBACH2 = 6;
-int ROUTE_A_FIXED = 7;
+int ROUTE_A_FIXATION = 7;
 int SIGNAL_A1 = 8;
 int SIGNAL_A2 = 9;
 
@@ -56,6 +55,10 @@ boolean current_position [ARRAY_SIZE] = { PLUS, PLUS, PLUS, PLUS, PLUS, PLUS, PL
 boolean blocked [ARRAY_SIZE] = { false, false, false, false, false, false, false, false, false, false };
 boolean requirements_fulfilled [ARRAY_SIZE] = { true, true, true, false, false, false, false, false, false, false };
 boolean position_is_ok [ARRAY_SIZE] = { true, true, true, true, true, true, true, true, true, true };
+
+
+// Actual levers/buttons/locks.
+Bounce levers [ARRAY_SIZE];
 
 
 boolean movement_allowed (int number) {
@@ -93,19 +96,19 @@ void update_requirements() {
   }
 
   // Requirements fulfilled by the route levers and/or the route fixer.
-  requirements_fulfilled[ROUTE_A_FIXED] = false;
+  requirements_fulfilled[ROUTE_A_FIXATION] = false;
   requirements_fulfilled[SIGNAL_A1] = false;
   requirements_fulfilled[SIGNAL_A2] = false;
   if (current_position[ROUTE_A1] == MINUS ||
       current_position[ROUTE_A2] == MINUS) {
-    requirements_fulfilled[ROUTE_A_FIXED] = true;
+    requirements_fulfilled[ROUTE_A_FIXATION] = true;
   }
   if (current_position[ROUTE_A2] == MINUS &&
-      current_position[ROUTE_A_FIXED] == MINUS) {
+      current_position[ROUTE_A_FIXATION] == MINUS) {
     requirements_fulfilled[SIGNAL_A2] = true;
   }
   if (current_position[ROUTE_A1] == MINUS &&
-      current_position[ROUTE_A_FIXED] == MINUS) {
+      current_position[ROUTE_A_FIXATION] == MINUS) {
     requirements_fulfilled[SIGNAL_A1] = true;
   }
 
@@ -117,8 +120,8 @@ void update_blocks() {
   // blocks a route lever.
 
   // Reset all blocks.
-  for (int i = 0; i < ARRAY_SIZE; i++) {
-    blocked[i] = false;
+  for (int number = 0; number < ARRAY_SIZE; number++) {
+    blocked[number] = false;
   }
 
   // Route levers block each other. And they block switches.
@@ -158,18 +161,25 @@ void update_blocks() {
   }
 
   // Route fixation blocks routes.
-  if (current_position[ROUTE_A_FIXED] == MINUS) {
+  if (current_position[ROUTE_A_FIXATION] == MINUS) {
     blocked[ROUTE_A1] = true;
     blocked[ROUTE_A2] = true;
     // Special case: also keep yourself blocked (it is only un-blocked by
     // moving a signal back to PLUS).
-    blocked[ROUTE_A_FIXED] = true;
+    blocked[ROUTE_A_FIXATION] = true;
   }
 }
 
 
 void change_position(int number,
                      int new_position) {
+  if (new_position == MINUS) {
+    digitalWrite(13, HIGH);
+  }
+  else {
+    digitalWrite(13, LOW);
+  }
+
   current_position[number] = new_position;
   position_is_ok[number] = true;
   update_requirements();
@@ -178,7 +188,7 @@ void change_position(int number,
   // Special case: signals going back to PLUS free the route fixation.
   if (number == SIGNAL_A1 || number == SIGNAL_A2) {
     if (new_position == PLUS) {
-      change_position(ROUTE_A_FIXED, PLUS);
+      change_position(ROUTE_A_FIXATION, PLUS);
     }
   }
 }
@@ -202,11 +212,94 @@ void react_to_movement(int number,
 }
 
 
+void update_single_lever_led(int pin, int number) {
+  if (position_is_ok[number] == false) {
+    if ((millis() % 1000) > 500) {
+      digitalWrite(pin, HIGH);
+    }
+    else {
+      digitalWrite(pin, LOW);
+    }
+  }
+  else {
+    if (movement_allowed(number)) {
+      digitalWrite(pin, LOW);
+    }
+    else {
+      digitalWrite(pin, HIGH);
+    }
+  }
+}
+
+/* void update_single_lever_led(int pin, int number) { */
+/*   if ((millis() % 1000) > 500) { */
+/*     digitalWrite(pin, HIGH); */
+/*   } */
+/*   else { */
+/*     digitalWrite(pin, LOW); */
+/*   } */
+/* } */
+
+// xxxx
+
 void setup() {
-  // TODO: attach the pins and hook up react_to_movement() functions.
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+
+  // Attach the pins and hook up react_to_movement() functions.
+  for (int number = 0; number < ARRAY_SIZE; number++) {
+    levers[number] = Bounce();
+  }
+
+  levers[SWITCH_5].attach(PIN_LEVER_SWITCH_5, INPUT_PULLUP);
+  levers[SWITCH_1].attach(PIN_LEVER_SWITCH_1, INPUT_PULLUP);
+  levers[SWITCHING_PERMISSION].attach(PIN_LOCK_SWITCHING_PERMISSION, INPUT_PULLUP);
+  levers[ROUTE_A1].attach(PIN_LEVER_ROUTE_A1, INPUT_PULLUP);
+  levers[ROUTE_A2].attach(PIN_LEVER_ROUTE_A2, INPUT_PULLUP);
+  levers[ROUTE_BERENBACH1].attach(PIN_LEVER_ROUTE_BERENBACH1, INPUT_PULLUP);
+  levers[ROUTE_BERENBACH2].attach(PIN_LEVER_ROUTE_BERENBACH2, INPUT_PULLUP);
+  levers[SIGNAL_A1].attach(PIN_LEVER_SIGNAL_A1, INPUT_PULLUP);
+  levers[SIGNAL_A2].attach(PIN_LEVER_SIGNAL_A2, INPUT_PULLUP);
+  levers[ROUTE_A_FIXATION].attach(PIN_BUTTON_ROUTE_A_FIXATION, INPUT_PULLUP);
+
+  for (int number = 0; number < ARRAY_SIZE; number++) {
+    levers[number].interval(20);
+  }
+  // Special case: route fixation button should take a second to activate.
+  levers[ROUTE_A_FIXATION].interval(1000);
+
+  // Attach the LEDs.
+  pinMode(PIN_LED_SWITCH_5, OUTPUT);
+  pinMode(PIN_LED_SWITCH_1, OUTPUT);
+  pinMode(PIN_LED_SIGNAL_A2, OUTPUT);
+  pinMode(PIN_LED_SIGNAL_A1, OUTPUT);
+  pinMode(PIN_LED_SWITCHING_PERMISSION, OUTPUT);
+  pinMode(PIN_LED_ROUTE_A, OUTPUT);
+  pinMode(PIN_LED_ROUTE_BERENBACH, OUTPUT);
+  pinMode(PIN_LED_ROUTE_A_FIXATION, OUTPUT);
+
+  // TODO: look up the initial lever positions and update accordingly.
 }
 
 
 void loop() {
-  // TODO: update the LEDs and update the outgoing pins.
+  for (int number = 0; number < ARRAY_SIZE; number++) {
+    levers[number].update();
+    if (levers[number].rose()) {
+      react_to_movement(number, PLUS);
+    }
+    if (levers[number].fell()) {
+      react_to_movement(number, MINUS);
+    }
+  }
+  // TODO: update the LEDs.
+  update_single_lever_led(PIN_LED_SWITCH_5, SWITCH_5);
+  update_single_lever_led(PIN_LED_SWITCH_1, SWITCH_1);
+  update_single_lever_led(PIN_LED_SIGNAL_A1, SIGNAL_A1);
+  update_single_lever_led(13, SIGNAL_A1);
+  update_single_lever_led(PIN_LED_SIGNAL_A2, SIGNAL_A2);
+  update_single_lever_led(PIN_LED_SWITCHING_PERMISSION, SWITCHING_PERMISSION);
+  // TODO: Update other three leds
+
+  // TODO: update the outgoing pins.
 }
