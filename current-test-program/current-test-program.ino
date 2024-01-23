9e7396efb1be38a3d222491861f1ea1501b19d0b
Reinout van Rees <reinout@vanrees.org>
2024-01-23 20:59:34 +0100

    Got a Detector class to work with a nice timeout

--- current-test-program/current-test-program.ino
+++ current-test-program/current-test-program.ino
@@ -1,31 +1,47 @@
 #include <Bounce2.h>
+#include "./detector.h"
 
 int RELAIS = 4;
-int IR_SENSOR_PIN = 5;
+int IR_SENSOR_PIN_TUNNEL = 5;
+int IR_SENSOR_PIN_CLOSE = 6;
+int PUSH_BUTTON_PIN = 7;
 
-Bounce ir_sensor = Bounce();
+Detector ir_sensor_tunnel = Detector(IR_SENSOR_PIN_TUNNEL);
+Bounce ir_sensor_close = Bounce();
+Bounce push_button = Bounce();
 
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(RELAIS, HIGH);
     pinMode(RELAIS, OUTPUT);
-    ir_sensor.attach(IR_SENSOR_PIN, INPUT_PULLUP);
-    ir_sensor.interval(50);
+    ir_sensor_close.attach(IR_SENSOR_PIN_CLOSE, INPUT_PULLUP);
+    ir_sensor_close.interval(50);
+    push_button.attach(PUSH_BUTTON_PIN, INPUT_PULLUP);
+    push_button.interval(50);
+    ir_sensor_tunnel.init();
 }
 
 void loop() {
-    ir_sensor.update();
-    if (ir_sensor.fell())  // fell: iets gedetecteerd, naar 0.
+    ir_sensor_tunnel.update();
+    ir_sensor_close.update();
+    push_button.update();
+    if (push_button.fell())  // fell: iets gedetecteerd, naar 0.
     {
-    digitalWrite(LED_BUILTIN, HIGH);
     digitalWrite(RELAIS, LOW);
     // low means 'on'
     }
 
-    if (ir_sensor.rose())
+    if (ir_sensor_close.fell())
     {
-    digitalWrite(LED_BUILTIN, LOW);
     digitalWrite(RELAIS, HIGH);
+    }
+
+    if (ir_sensor_tunnel.active){
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else
+    {
+      digitalWrite(LED_BUILTIN, LOW);
     }
 }
