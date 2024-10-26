63ca71039a72befdf84dbfa728860b3d422ec98a
Reinout van Rees <reinout@vanrees.org>
2024-10-26 18:20:12 +0200

    Got basics working a bit

--- turntables/mayen/mayen.ino
+++ turntables/mayen/mayen.ino
@@ -5,17 +5,25 @@
  */
 
 #include <AccelStepper.h>
+#include <Bounce2.h>
 
-#define ENABLE_PIN 3
-#define DIR_PIN 4
-#define STEP_PIN 5
+// Pins
+int ENABLE_PIN = 3;
+int DIR_PIN = 4;
+int STEP_PIN = 5;
+int END_STOP_PIN = 7;
 
-#define STEPS_PER_ROTATION 200
-#define MICROSTEPPING_FACTOR 2
+// Handy constants
+long PHYSICAL_STEPS_PER_ROTATION = 200;
+long MICROSTEPPING_FACTOR = 2;
+long STEPS_PER_ROTATION = PHYSICAL_STEPS_PER_ROTATION * MICROSTEPPING_FACTOR;
 
-
-// Define the stepper motor and the pins that is connected to
+// Buttons, stepper motors
 AccelStepper motor(1, STEP_PIN, DIR_PIN); // (Type of driver: with 2 pins, STEP, DIR)
+Bounce end_stop = Bounce();
+
+// States
+boolean am_homing = true;
 
 
 void enable_motor() {
@@ -36,14 +44,31 @@ void configure_motor_for_operation() {
   motor.setAcceleration(400);
 }
 
+void configure_zero_point() {
+  motor.setCurrentPosition(0);
+  am_homing = false;
+  configure_motor_for_operation();
+  motor.moveTo(-10);
+}
+
 void setup() {
   pinMode(ENABLE_PIN, OUTPUT);
-  configure_motor_for_operation();
-  motor.moveTo(STEPS_PER_ROTATION * MICROSTEPPING_FACTOR * 10);
+  end_stop.attach(END_STOP_PIN, INPUT_PULLUP);
+  end_stop.interval(1);
+  configure_motor_for_homing();
+  motor.moveTo(STEPS_PER_ROTATION * 1);
   enable_motor();
 }
 
 void loop() {
+  end_stop.update();
+  if (am_homing and end_stop.fell())
+    {
+      configure_zero_point();
+      // Temp hack
+      motor.moveTo(STEPS_PER_ROTATION * -10);
+    }
+
   if (motor.distanceToGo() == 0)
     {
       disable_motor();
